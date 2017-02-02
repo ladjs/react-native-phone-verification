6f3bd537dd4a1692540f3825d79a6015c44a408d
Nick Baugh <niftylettuce@gmail.com>
2017-02-02 06:39:29 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,74 @@
+
+# React Native Phone Verification
+
+[![PayPal Donate][paypal-donate-image]][paypal-donate-url]
+[![Slack Status][slack-image]][slack-url]
+[![Standard JS Style][standard-image]][standard-url]
+[![MIT License][license-image]][license-url]
+
+The best React Native example for phone verification (an alternative to [Twitter Digits][twitter-digits]). This package was built for [CrocodileJS][crocodile-url].
+
+![React Native Phone Verification][demo]
+
+## Features
+
+* :boom: Works with iOS and Android! :100: 100% Cross-platform baby :baby:!
+* :flags: Built-in country picker (uses [react-native-country-picker-modal][react-native-country-picker-modal])!
+* :wrench: Completely customizable form (uses [react-native-form][react-native-form], but you could also use [tcomb-form-native][tcomb-form-native])!
+* :crystal_ball: Spinner overlay (uses [react-native-loading-spinner-overlay][react-native-loading-spinner-overlay])!
+* :sparkles: Optional Android support for reading verification codes automatically (uses [react-native-android-sms-listener][react-native-android-sms-listener])!
+
+## Usage
+
+1. Install [React Native][react-native]:
+
+  ```bash
+  npm install -g react-native
+  ```
+
+2. Initialize a new project:
+
+  ```bash
+  react-native init myproject
+  ```
+
+3. Install dependencies:
+
+  > If you're using `npm` to install packages:
+
+  ```bash
+  npm install --save frisbee react-native-android-sms-listener niftylettuce/react-native-country-picker-modal react-native-form react-native-loading-spinner-overlay
+  ```
+
+  > Or, if you're using `yarn` to install packages:
+
+  ```bash
+  yarn add frisbee react-native-android-sms-listener niftylettuce/react-native-country-picker-modal react-native-form react-native-loading-spinner-overlay
+  ```
+
+4. See [example][example] directory for how to integrate into your codebase (you can also just clone this repo and run the example to test it out).
+
+
+## License
+
+All code is licensed under [MIT][license-url]
+
+
+[paypal-donate-image]: https://img.shields.io/badge/paypal-donate-orange.svg
+[paypal-donate-url]: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FE3EFQ5X9RHT6
+[license-image]: http://img.shields.io/badge/license-MIT-blue.svg
+[license-url]: LICENSE
+[crocodile-url]: https://crocodilejs.com
+[slack-image]: http://slack.crocodilejs.com/badge.svg
+[slack-url]: http://slack.crocodilejs.com
+[standard-image]: https://img.shields.io/badge/code%20style-standard%2Bes7-brightgreen.svg
+[standard-url]: https://github.com/crocodilejs/eslint-config-crocodile
+[react-native-country-picker-modal]: https://github.com/xcarpentier/react-native-country-picker-modal
+[react-native]: https://facebook.github.io/react-native/
+[react-native-android-sms-listener]: https://github.com/CentaurWarchief/react-native-android-sms-listener
+[tcomb-form-native]: https://github.com/gcanti/tcomb-form-native
+[react-native-form]: https://github.com/julianocomg/react-native-form
+[react-native-loading-spinner-overlay]: https://github.com/niftylettuce/react-native-loading-spinner-overlay
+[demo]: https://cdn.rawgit.com/niftylettuce/react-native-phone-verification/master/media/example.png
+[example]: example/index.ios.js
+[twitter-digits]: https://get.digits.com/
---
+++ example/index.ios.js
@@ -0,0 +1,325 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Platform,
+  Alert
+} from 'react-native';
+
+import Frisbee from 'frisbee';
+import Spinner from 'react-native-loading-spinner-overlay';
+import SmsListener from 'react-native-android-sms-listener';
+import Form from 'react-native-form';
+import CountryPicker from 'react-native-country-picker-modal';
+
+const api = new Frisbee({
+  baseURI: 'http://localhost:3000',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+});
+
+// if you want to customize the country picker
+const countryPickerCustomStyles = {};
+
+// your brand's theme primary color
+const brandColor = '#744BAC';
+
+const styles = StyleSheet.create({
+  countryPicker: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  container: {
+    flex: 1
+  },
+  header: {
+    textAlign: 'center',
+    marginTop: 60,
+    fontSize: 22,
+    margin: 20,
+    color: '#4A4A4A',
+  },
+  form: {
+    margin: 20
+  },
+  textInput: {
+    padding: 0,
+    margin: 0,
+    flex: 1,
+    fontSize: 20,
+    color: brandColor
+  },
+  button: {
+    marginTop: 20,
+    height: 50,
+    backgroundColor: brandColor,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontFamily: 'Helvetica',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  wrongNumberText: {
+    margin: 10,
+    fontSize: 14,
+    textAlign: 'center'
+  },
+  disclaimerText: {
+    marginTop: 30,
+    fontSize: 12,
+    color: 'grey'
+  },
+  callingCodeView: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  callingCodeText: {
+    fontSize: 20,
+    color: brandColor,
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
+    paddingRight: 10
+  }
+});
+
+export default class example extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      enterCode: false,
+      spinner: false,
+      country: {
+        cca2: 'US',
+        callingCode: '1'
+      }
+    };
+  }
+
+  _getCode = () => {
+
+    this.setState({ spinner: true });
+
+    setTimeout(async () => {
+
+      try {
+
+        const res = await api.post('/v1/verifications', {
+          body: {
+            ...this.refs.form.getValues(),
+            ...this.state.country
+          }
+        });
+
+        if (res.err) throw res.err;
+
+        this.setState({
+          spinner: false,
+          enterCode: true,
+          verification: res.body
+        });
+        this.refs.form.refs.textInput.setNativeProps({ text: '' });
+        this.refs.form.refs.textInput.focus();
+
+        setTimeout(() => {
+          Alert.alert('Sent!', "We've sent you a verification code");
+        }, 100);
+
+      } catch (err) {
+        // <https://github.com/niftylettuce/react-native-loading-spinner-overlay/issues/30#issuecomment-276845098>
+        this.setState({ spinner: false });
+        setTimeout(() => {
+          Alert.alert('Oops!', err.message);
+        }, 100);
+      }
+
+    }, 100);
+
+  }
+
+  _verifyCode = () => {
+
+    this.setState({ spinner: true });
+
+    setTimeout(async () => {
+
+      try {
+
+        const res = await api.put('/v1/verifications', {
+          body: {
+            ...this.refs.form.getValues(),
+            ...this.state.country
+          }
+        });
+
+        if (res.err) throw res.err;
+
+        this.refs.form.refs.textInput.blur();
+        // <https://github.com/niftylettuce/react-native-loading-spinner-overlay/issues/30#issuecomment-276845098>
+        this.setState({ spinner: false });
+        setTimeout(() => {
+          Alert.alert('Success!', 'You have successfully verified your phone number');
+        }, 100);
+
+      } catch (err) {
+        // <https://github.com/niftylettuce/react-native-loading-spinner-overlay/issues/30#issuecomment-276845098>
+        this.setState({ spinner: false });
+        setTimeout(() => {
+          Alert.alert('Oops!', err.message);
+        }, 100);
+      }
+
+    }, 100);
+
+  }
+
+  _tryAgain = () => {
+    this.refs.form.refs.textInput.setNativeProps({ text: '' })
+    this.refs.form.refs.textInput.focus();
+    this.setState({ enterCode: false });
+  }
+
+  _getSubmitAction = () => {
+    this.state.enterCode ? this._verifyCode() : this._getCode();
+  }
+
+  _changeCountry = (country) => {
+    this.setState({ country });
+    this.refs.form.refs.textInput.focus();
+  }
+
+  _renderFooter = () => {
+
+    if (this.state.enterCode)
+      return (
+        <View>
+          <Text style={styles.wrongNumberText} onPress={this._tryAgain}>
+            Enter the wrong number or need a new code?
+          </Text>
+        </View>
+      );
+
+    return (
+      <View>
+        <Text style={styles.disclaimerText}>By tapping "Send confirmation code" above, we will send you an SMS to confirm your phone number. Message &amp; data rates may apply."</Text>
+      </View>
+    );
+
+  }
+
+  _renderCountryPicker = () => {
+
+    if (this.state.enterCode)
+      return (
+        <View />
+      );
+
+    return (
+      <CountryPicker
+        ref={'countryPicker'}
+        closeable
+        style={styles.countryPicker}
+        onChange={this._changeCountry}
+        cca2={this.state.country.cca2}
+        styles={countryPickerCustomStyles}
+        translation='eng'/>
+    );
+
+  }
+
+  _renderCallingCode = () => {
+
+    if (this.state.enterCode)
+      return (
+        <View />
+      );
+
+    return (
+      <View style={styles.callingCodeView}>
+        <Text style={styles.callingCodeText}>+{this.state.country.callingCode}</Text>
+      </View>
+    );
+
+  }
+
+  render() {
+
+    let headerText = `What's your ${this.state.enterCode ? 'verification code' : 'phone number'}?`
+    let buttonText = this.state.enterCode ? 'Verify confirmation code' : 'Send confirmation code';
+    let textStyle = this.state.enterCode ? {
+      height: 50,
+      textAlign: 'center',
+      fontSize: 40,
+      fontWeight: 'bold',
+      fontFamily: 'Courier'
+    } : {};
+
+    return (
+
+      <View style={styles.container}>
+
+        <Text style={styles.header}>{headerText}</Text>
+
+        <Form ref={'form'} style={styles.form}>
+
+          <View style={{ flexDirection: 'row' }}>
+
+            {this._renderCountryPicker()}
+            {this._renderCallingCode()}
+
+            <TextInput
+              ref={'textInput'}
+              name={this.state.enterCode ? 'code' : 'phoneNumber' }
+              type={'TextInput'}
+              underlineColorAndroid={'transparent'}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+              placeholder={this.state.enterCode ? '_ _ _ _ _ _' : 'Phone Number'}
+              keyboardType={Platform.OS === 'ios' ? 'number-pad' : 'numeric'}
+              style={[ styles.textInput, textStyle ]}
+              returnKeyType='go'
+              autoFocus
+              placeholderTextColor={brandColor}
+              selectionColor={brandColor}
+              maxLength={this.state.enterCode ? 6 : 20}
+              onSubmitEditing={this._getSubmitAction} />
+
+          </View>
+
+          <TouchableOpacity style={styles.button} onPress={this._getSubmitAction}>
+            <Text style={styles.buttonText}>{ buttonText }</Text>
+          </TouchableOpacity>
+
+          {this._renderFooter()}
+
+        </Form>
+
+        <Spinner
+          visible={this.state.spinner}
+          textContent={'One moment...'}
+          textStyle={{ color: '#fff' }} />
+
+      </View>
+
+    );
+  }
+}
+
+AppRegistry.registerComponent('example', () => example);
